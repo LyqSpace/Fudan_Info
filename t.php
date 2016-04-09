0ae55fb413b40157aea6cdddfe199ef96366d657
LyqSpace <root@lyq.me>
2016-04-10 07:50:04 +0800

    temp

--- t.php
+++ t.php
@@ -18,7 +18,7 @@ if (isset($_GET['t']) && $_GET['t'] != "") {
     $mysql = mysql_connect("localhost", "root", "Xmlyqing2016");
     mysql_query("set names 'utf8'");
     mysql_select_db("fudan_info");
-    
+
     $query = sprintf('select * from event_info where short_url="%s";', $_GET['t']);
     $res = mysql_query($query);
     if (mysql_num_rows($res)) {
