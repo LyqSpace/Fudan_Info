fc16ad178cd42771f5848c2e4effef8490da223c
LyqSpace <root@lyq.me>
2016-06-04 18:02:17 +0800

    disabled publish system

--- index.php
+++ index.php
@@ -42,6 +42,11 @@ if (isset($_COOKIE['login_serial'])) {
 <body ontouchstart>
 <div class="page_header">
     <h1 class="page_title">FDUTOPIA</h1>
+    <p class="page_desc">小伙伴们，我们进入夏眠啦</p>
+    <p class="page_desc">系统将在暑假结束前开放</p>
+    <p class="page_desc">届时欢迎使用非常赞的<span id="info_green">票务系统</span></p>
+    <p class="page_desc">祝小伙伴们期末考试顺利！^_^</p>
+    <br>
     <p class="page_desc">活动信息将在<span class="text_warn">每周日晚六点</span>被推送</p>
     <p class="page_desc">招新信息只在<span class="text_warn">每学期前四周</span>被推送</p>
     <p class="page_desc">已入驻的主办方数 : <?php
@@ -68,8 +73,8 @@ if (isset($_COOKIE['login_serial'])) {
 <!--    <script type="text/javascript" src="check_html5.js"></script>-->
 
     <div class="weui_btn_area">
-        <a class="weui_btn weui_btn_plain_primary" href="edit_event.php">发布一则活动信息</a>
-        <a class="weui_btn weui_btn_plain_primary" href="edit_recruit.php">发布一则招新信息</a>
+        <a class="weui_btn weui_btn_plain_primary disabled">发布一则活动信息</a>
+        <a class="weui_btn weui_btn_plain_primary disabled">发布一则招新信息</a>
         <a class="weui_btn weui_btn_plain_primary" href="registeration_list.php">查看我的票务系统</a>
         <a class="weui_btn weui_btn_plain_primary" href="client_history.php">查看我的历史发布</a>
         <a class="weui_btn weui_btn_plain_primary" href="edit_profile.php">修改我的基本信息</a>
